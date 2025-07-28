e775d2ee7c918e030411126fc391f03e84877fa8
Father Amal <fatheram@le-a5.nov.21-school.ru>
2025-07-28 23:12:03 +0300

    added quick sort

--- sorting_algorithms.c
+++ sorting_algorithms.c
@@ -7,13 +7,12 @@ void merge(int *b, int left, int mid, int right);
 void merge_sort(int *b, int left, int right);
 void heap_sort(int *c);
 void heapify(int *c, int n, int i);
-// void quick_sort();
+void quick_sort(int *d, int left, int right);
 void output(int *a, int *b, int *c);
 
 int main() {
     int data[NMAX];
-    int data_bs[NMAX], data_ms[NMAX], data_hs[NMAX];
-    // int data_qs[NMAX];
+    int data_bs[NMAX], data_ms[NMAX], data_hs[NMAX], data_qs[NMAX];
     for (int *p = data; p - data < NMAX; ++p) {
         scanf("%d", p);
     }
@@ -22,13 +21,13 @@ int main() {
         data_bs[i] = data[i];
         data_ms[i] = data[i];
         data_hs[i] = data[i];
-        // data_qs[i] = data[i];
+        data_qs[i] = data[i];
     }
 
     bubble_sort(data_bs);
     merge_sort(data_ms, 0, NMAX - 1);
     heap_sort(data_hs);
-    // quick_sort(data_qs);
+    quick_sort(data_qs, 0, NMAX - 1);
     output(data_bs, data_ms, data_hs);
 }
 
@@ -132,6 +131,28 @@ void heapify(int *c, int n, int i) {
 
         heapify(c, n, largest);
     }
+}
+
+void quick_sort(int *d, int left, int right) {
+    if (left >= right) return;
+
+    int p = d[(left + right) / 2];
+    int i = left;
+    int j = right;
+
+    while (i <= j) {
+        while (d[i] <= p) ++i;
+        while (d[j] >= p) --j;
+
+        if (i <= j) {
+            swap(&d[i], &d[j]);
+            ++i;
+            --j;
+        }
+    }
+
+    quick_sort(d, left, i);
+    quick_sort(d, j, right);
 }
 
 void output(int *a, int *b, int *c) {
